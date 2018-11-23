6648c70d60891e8d2fc3f7efc917a68d13e1b91a
longl <longlong2l@163.com>
2018-11-23 17:26:25 +0800

    组件化：common_base

--- app/src/main/java/com/study/longl/myselfviewdemo/RefreshRecyclerView/NormalAdapter.java
+++ app/src/main/java/com/study/longl/myselfviewdemo/RefreshRecyclerView/NormalAdapter.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
